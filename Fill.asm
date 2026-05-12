f212b2adb7c34cdece84e556ae194b13db94f79b
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2026-05-12 22:29:35 +0900

    Fill.asm: init

---
+++ Fill.asm
@@ -0,0 +1,9 @@
+/**
+    *LOOP:
+    * read keyboard
+    * if no key -> color = 0
+    * if key    -> color = -1
+    * reset addr to SCREEN
+    * write color from SCREEN until KBD
+    * repeat
+*/
