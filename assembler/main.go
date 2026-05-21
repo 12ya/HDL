9887e07da98996c4660284c2992f1ce629e715b8
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2026-05-21 16:21:16 +0900

    assembler/main.go: add tables, and cleanLine func

--- assembler/main.go
+++ assembler/main.go
@@ -1,9 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
 	// 1. read .asm file
 	// 2. clean lines
 	// 3. pass 1: labels
 	// 4. pass 2: translate
 	// 5. write .hack file
+
+	symbolTable := map[string]int{
+		"SP":     0,
+		"LCL":    1,
+		"ARG":    2,
+		"THIS":   3,
+		"SCREEN": 16384,
+		"KBD":    24576,
+	}
+
+	for i := 0; i <= 15; i++ {
+		symbolTable[fmt.Sprintf("R%d", i)] = i
+	}
+
+	destTable := map[string]string{
+		"":    "000",
+		"M":   "001",
+		"D":   "010",
+		"DM":  "011",
+		"A":   "100",
+		"AM":  "101",
+		"AD":  "110",
+		"AMD": "111",
+	}
+
+	jumpTable := map[string]string{
+		"":    "000",
+		"JGT": "001",
+		"JEQ": "010",
+		"JGE": "011",
+		"JLT": "100",
+		"JNE": "101",
+		"JLE": "110",
+		"JMP": "111",
+	}
+
+}
+
+func cleanLine(line string) string {
+	line, _, _ = strings.Cut(line, "//")
+	line = strings.TrimSpace(line)
+	line = strings.ReplaceAll(line, " ", "")
+	return line
 }
